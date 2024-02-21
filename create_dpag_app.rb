7d9ab86840edefbe9fe33f39d364bf687e86c833
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-02-21 21:39:45 +0100

    add setup.py

--- create_dpag_app.rb
+++ create_dpag_app.rb
@@ -1,15 +1,30 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CreateDpagApp < Formula
+  include Language::Python::Virtualenv
+
   desc "A simple tool to clone GitHub repos into a directory with the same name"
   homepage "https://github.com/LundIT/create-dpag-app"
-  url "https://github.com/LundIT/create-dpag-app/archive/v0.0.1.tar.gz"
+  url "https://github.com/LundIT/create-dpag-app/archive/v0.0.2.tar.gz"
+  sha256 "f9d8312cda90889891a846ff3ddd246ccd1d8e420a07b53404284980dfb90d0c"
 
-  depends_on "python@3.9"
+  depends_on "python"
 
   def install
-    bin.install "create_dpag_app.py" => "create-dpag-app"
+    virtualenv_install_with_resources
   end
 
   test do
-    system "#{bin}/create-dpag-app", "--version"
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test create-dpag-app`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="create-dpag-app",
+    version="1.0.0",
+    author="Melih Sünbül",
+    author_email="m.sunbul@lund-it.com",
+    description="A Python / Django library to create business applications easily with complex logic",
+    long_description_content_type="text/markdown",
+    url="https://github.com/LundIT/create-dpag-app",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
