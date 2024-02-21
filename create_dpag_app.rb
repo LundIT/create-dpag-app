3adbaa3705e2c67623e0800b6eca6b0337814775
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2024-02-21 18:08:51 +0100

    fix try

--- create_dpag_app.rb
+++ create_dpag_app.rb
@@ -2,9 +2,8 @@ class CreateDpagApp < Formula
   desc "A simple tool to clone GitHub repos into a directory with the same name"
   homepage "https://github.com/LundIT/create-dpag-app"
   url "https://github.com/LundIT/create-dpag-app/archive/v0.0.1.tar.gz"
-  sha256 "your_tarball's_sha256_checksum"
 
-  depends_on "python@3.10"
+  depends_on "python@3.9"
 
   def install
     bin.install "create_dpag_app.py" => "create-dpag-app"
